6ed7adab346a8d2744bb81647393933d8fe0f24d
Boyd H. Ako <Boyd.Hanalei.Ako@gmail.com>
2020-07-03 00:44:03 -1000

    Updating from generate_PrivateInternetAccess_config.bash

--- refresh_all_rules.bash
+++ refresh_all_rules.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd $(dirname $0)
 for script in $(find $PWD -mindepth 2 -type f -iname "*.bash"); do
+	printf "\n===== %s =====\n" "$script"
 	sname="$(basename $script)"
 	$script
+	printf "\n##### commiting %s #####\n" "$script"
 	git commit -a -m "Updating from $sname"
 	git push
 done
