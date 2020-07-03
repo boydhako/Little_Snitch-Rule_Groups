ec966512c2ec20e3d2527f419409c7ef9684577b
Boyd H. Ako <Boyd.Hanalei.Ako@gmail.com>
2020-07-02 22:15:40 -1000

    Updating Google Chrome rules script

--- Google_Chrome/Google_Chrome-lsrules.bash
+++ Google_Chrome/Google_Chrome-lsrules.bash
@@ -12,7 +12,7 @@ function GCHROMERULES {
 		for bin in $(find "$gcdir" -type f -name "Google Chrome Helper" | sed 's~ ~_~g'); do
 			gbin="$(echo $bin | sed -e 's~/~\\/~g' -e 's~_~ ~g')"
 			for proto in UDP TCP; do
-				printf "\t{\n\t\t\"action\" : \"allow\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"443\",\n\t\t\"via\" : \"%s\"\n\t\t\"remote\" : \"any\"\n\t},\n" "$proto" "$proc" "$gbin"
+				printf "\t{\n\t\t\"action\" : \"allow\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"443\",\n\t\t\"via\" : \"%s\",\n\t\t\"remote\" : \"any\"\n\t},\n" "$proto" "$proc" "$gbin"
 			done
 		done
 	fi
