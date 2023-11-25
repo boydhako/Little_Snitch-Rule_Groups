f23dd6672e70f2b8395defacf42fc909e464dac8
Boyd H. Ako <boyd.hanalei.ako@gmail.com>
2023-11-25 12:40:13 -1000

    Updating from generate_PrivateInternetAccess_config.bash

--- refresh_all_rules.bash
+++ refresh_all_rules.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd $(dirname $0)
 for script in $(find $PWD -mindepth 2 -type f -iname "*.bash"); do
+    git pull
 	printf "\n===== %s =====\n" "$script"
 	sname="$(basename $script)"
 	$script
