94c9d9636296c842d20b53a3da4317c23a6a0a16
Boyd H. Ako <boyd.hanalei.ako@gmail.com>
2023-12-07 23:55:13 -1000

    Updating from generate_PrivateInternetAccess_config.bash

--- refresh_all_rules.bash
+++ refresh_all_rules.bash
@@ -7,6 +7,5 @@ for script in $(find $PWD -mindepth 2 -type f -iname "*.bash"); do
 	$script
 	printf "\n##### commiting %s #####\n" "$script"
 	git commit -a -m "Updating from $sname"
-	git push
 done
 git push
