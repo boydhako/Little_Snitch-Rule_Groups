c5b338f7b3448b49dd5f6431159f7c8be17506f8
Boyd H. Ako <boyd.hanalei.ako@gmail.com>
2023-12-10 22:09:27 -1000

    changed remote-domains to remote-hosts in generate_PrivateInternetAccess_config.bash

--- PrivateInternetAccess/Tunnelblick/generate_PrivateInternetAccess_config.bash
+++ PrivateInternetAccess/Tunnelblick/generate_PrivateInternetAccess_config.bash
@@ -34,7 +34,7 @@ function PIAGENCFG {
 	for vpnbin in $ovpn; do
 		for host in $servers; do
             for direction in incoming outgoing; do
-                printf "\t\t{\n\t\t\"action\" : \"allow\",\n\t\t\"direction\" : \"%s\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"remote-domains\" : \"%s\"\n\t\t},\n" "$direction" "$proto" "$vpnbin" "$host" >> $lsrule
+                printf "\t\t{\n\t\t\"action\" : \"allow\",\n\t\t\"direction\" : \"%s\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"remote-hosts\" : \"%s\"\n\t\t},\n" "$direction" "$proto" "$vpnbin" "$host" >> $lsrule
             done
 		done
 	done
