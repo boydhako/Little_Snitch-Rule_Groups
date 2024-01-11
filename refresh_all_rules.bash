d2dfe5a0e3f04a5edb92f91ee1cf4edbdfd67015
Boyd H. Ako <boyd.hanalei.ako@gmail.com>
2024-01-10 21:20:00 -1000

    Updating from generate_PrivateInternetAccess_config.bash

--- refresh_all_rules.bash
+++ refresh_all_rules.bash
@@ -8,7 +8,7 @@ for script in $(find $PWD -mindepth 2 -type f -iname "*.bash"); do
 	printf "\n##### commiting %s #####\n" "$script"
 	git commit -a -m "Updating from $sname"
     printf "EXITCODE:%s\n" "$?"
-    if [ "$?" == "1" ]; then
+    if [ "$?" == "0" ]; then
         export push="1"
     fi
 done
