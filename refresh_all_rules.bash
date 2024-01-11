592388118a8796967a5b4431db9ef28c9e53909f
Boyd H. Ako <boyd.hanalei.ako@gmail.com>
2024-01-10 21:33:47 -1000

    updated refresh_all_rules.bash

--- refresh_all_rules.bash
+++ refresh_all_rules.bash
@@ -9,7 +9,6 @@ for script in $(find $PWD -mindepth 2 -type f -iname "*.bash"); do
 	printf "\n##### commiting %s #####\n" "$script"
 	git commit -a -m "Updating from $sname"
     exitcode="$?"
-    printf "EXITCODE:%s\n" "$exitcode"
     if [ "$exitcode" == "0" ]; then
         export push="1"
     fi
@@ -17,4 +16,3 @@ done
 if [ "$push" == "1" ]; then
     git push
 fi
-unset push
