b6f3df302370bb66e2cd6e367e73ce64a45d9562
Boyd H. Ako <boyd.hanalei.ako@gmail.com>
2024-01-12 00:22:58 -1000

    move commit output to only if something was commited

--- refresh_all_rules.bash
+++ refresh_all_rules.bash
@@ -10,11 +10,11 @@ for script in $(find $PWD -mindepth 2 -type f -iname "*.bash"); do
 	printf "\n===== %s =====\n" "$script"
 	sname="$(basename $script)"
 	$script
-	printf "\n##### commiting %s #####\n" "$script"
 	git commit -a -m "Updating from $sname"
     exitcode="$?"
     if [ "$exitcode" == "0" ]; then
         export push="1"
+        printf "\n##### commiting %s #####\n" "$script"
     fi
 done
 if [ "$push" == "1" ]; then
