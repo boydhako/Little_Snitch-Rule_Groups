555420d227f0c648d5f8a6424cc33deee1efb6a4
Boyd H. Ako <boyd.hanalei.ako@gmail.com>
2024-02-17 19:42:40 -1000

    Updating from generate_PrivateInternetAccess_config.bash

--- Google_Chrome/Google_Chrome-lsrules.bash
+++ Google_Chrome/Google_Chrome-lsrules.bash
@@ -1,24 +1,30 @@
-#!/bin/bash 
-gcdir="/Applications/Google Chrome.app"
-
+#!/bin/bash
+appdir="/Applications"
+gc="$(find "$appdir" -type f -name "Google Chrome")"
+gcdir="$(dirname "$gc" | sed 's#/Contents/.*##g')"
+gch="$(find "$gcdir" -type f -name "Google Chrome Helper")"
 function GCHROMEHEAD {
 	printf "{\n\t\"name\": \"Google Chrome\",\n\t\"description\": \"Google Chrome keeps changing it\'s path\",\n\t\"rules\": [\n"
 }
 
 function GCHROMERULES {
-	if [ -d "$gcdir" ]; then
-		proc="$(find "$gcdir" -name "Google Chrome" | sed -e 's~/~\\/~g')"
-		#find "$gcdir" -type f -name "Google Chrome Helper" | sed 's~/~\\/~g'
-		for bin in $(find "$gcdir" -type f -name "Google Chrome Helper" | sed 's~ ~_~g'); do
-			gbin="$(echo $bin | sed -e 's~/~\\/~g' -e 's~_~ ~g')"
-			for proto in UDP TCP; do
-				for dport in 443 5228; do
-					printf "\t{\n\t\t\"action\" : \"allow\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"via\" : \"%s\",\n\t\t\"remote\" : \"any\"\n\t},\n" "$proto" "$proc" "$dport" "$gbin"
-				done
-			done
-			printf "\t{\n\t\t\"action\" : \"allow\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"via\" : \"%s\",\n\t\t\"remote-hosts\" : \"redirector.gvt1.com\"\n\t},\n" "TCP" "$proc" "80" "$gbin"
+	echo $gc
+	echo $gcdir
+	echo $gch
+	for port in 80 443 5228; do
+		for protocol in TCP UDP; do
+			if [ "$port" == "5228" -a "$protocol" == "UDP" ]; then
+				printf "\t{\n\t\t\"action\" : \"deny\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"remote\" : \"any\"\n\t}," "$protocol" "$gc" "$port"
+				printf "\t{\n\t\t\"action\" : \"deny\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"via\" : \"%s\"\n\t\t\"remote\" : \"any\"\n\t}," "$protocol" "$gc" "$port" "$gch"
+			elif [ "$port" == "80" ]; then
+				printf "\t{\n\t\t\"action\" : \"allow\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"remote\" : \"redirector.gvt1.com\"\n\t}," "$protocol" "$gc" "$port"
+				printf "\t{\n\t\t\"action\" : \"allow\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"via\" : \"%s\"\n\t\t\"remote\" : \"redirector.gvt1.com\"\n\t}," "$protocol" "$gc" "$port" "$gch"
+			else
+				printf "\t{\n\t\t\"action\" : \"allow\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"remote\" : \"any\"\n\t}," "$protocol" "$gc" "$port"
+				printf "\t{\n\t\t\"action\" : \"allow\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"via\" : \"%s\"\n\t\t\"remote\" : \"any\"\n\t}," "$protocol" "$gc" "$port" "$gch"
+			fi
 		done
-	fi
+	done
 }
 
 function GCHROMEFOOT {
@@ -31,3 +37,4 @@ function GCHROME {
 	GCHROMEFOOT
 }
 GCHROME > $(basename -s.bash $0)
+#GCHROME
