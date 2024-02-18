475f82668bae8e1a8e07656b5ad7c17b7490e86d
Boyd H. Ako <boyd.hanalei.ako@gmail.com>
2024-02-17 19:55:38 -1000

    Updating from generate_PrivateInternetAccess_config.bash

--- Google_Chrome/Google_Chrome-lsrules.bash
+++ Google_Chrome/Google_Chrome-lsrules.bash
@@ -12,13 +12,13 @@ function GCHROMERULES {
 		for protocol in TCP UDP; do
 			if [ "$port" == "5228" -a "$protocol" == "UDP" ]; then
 				printf "\t{\n\t\t\"action\" : \"deny\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"remote\" : \"any\"\n\t}," "$protocol" "$gc" "$port"
-				printf "\t{\n\t\t\"action\" : \"deny\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"via\" : \"%s\"\n\t\t\"remote\" : \"any\"\n\t}," "$protocol" "$gc" "$port" "$gch"
+				printf "\t{\n\t\t\"action\" : \"deny\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"via\" : \"%s\",\n\t\t\"remote\" : \"any\"\n\t}," "$protocol" "$gc" "$port" "$gch"
 			elif [ "$port" == "80" ]; then
 				printf "\t{\n\t\t\"action\" : \"allow\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"remote\" : \"redirector.gvt1.com\"\n\t}," "$protocol" "$gc" "$port"
-				printf "\t{\n\t\t\"action\" : \"allow\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"via\" : \"%s\"\n\t\t\"remote\" : \"redirector.gvt1.com\"\n\t}," "$protocol" "$gc" "$port" "$gch"
+				printf "\t{\n\t\t\"action\" : \"allow\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"via\" : \"%s\",\n\t\t\"remote\" : \"redirector.gvt1.com\"\n\t}," "$protocol" "$gc" "$port" "$gch"
 			else
 				printf "\t{\n\t\t\"action\" : \"allow\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"remote\" : \"any\"\n\t}," "$protocol" "$gc" "$port"
-				printf "\t{\n\t\t\"action\" : \"allow\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"via\" : \"%s\"\n\t\t\"remote\" : \"any\"\n\t}," "$protocol" "$gc" "$port" "$gch"
+				printf "\t{\n\t\t\"action\" : \"allow\",\n\t\t\"protocol\" : \"%s\",\n\t\t\"process\" : \"%s\",\n\t\t\"ports\" : \"%s\",\n\t\t\"via\" : \"%s\"\n,\t\t\"remote\" : \"any\"\n\t}," "$protocol" "$gc" "$port" "$gch"
 			fi
 		done
 	done
